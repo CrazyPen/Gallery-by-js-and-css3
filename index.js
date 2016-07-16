9fe3963238279d1a59c2ae22130e9a695d0f56a7
YYB <819662058@qq.com>
2016-07-16 17:00:26 +0800

    add nav control

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-// var Gallery = {
-// 	height: document.body.clientHeight,
-// 	width: document.body.clientWidth,
-// 	content: document.getElementById("Gallery"),
-// 	imgs: document.getElementsByClassName("img-cell"),
-// 	placeCenter: function (i) {
-// 			imgs[i].style.top = (height/2-150)+"px";
-// 			imgs[i].style.left = (width/2-190)+"px";
-// 			imgs[i].style.transform = "rotate(0deg)";
-// 		}
-// }
-
 var height = document.body.clientHeight;
 var width = document.body.clientWidth;
+var pic_width = 320;
+var pic_height = 260;
 
-var gallery = document.getElementById("gallery");
-var imgs = document.getElementsByClassName("cell");
 var centerpic = Math.floor(Math.random()*imgData.length);
 var isback = false;
 arrageImg();
@@ -23,19 +11,21 @@ addEvent();
 
 
 function placeCenter(i) {
-	imgs[i].style.top = (height/2 - 150)+"px";
-	imgs[i].style.left = (width/2 - 190)+"px";
+	var imgs = document.getElementsByClassName("cell");
+	imgs[i].style.top = (height/2 - 130)+"px";
+	imgs[i].style.left = (width/2 - 160)+"px";
 	imgs[i].style.transform = "rotate(0deg)";
 	imgs[i].style.zIndex = 100;
-}
-function placeRandom(i){
-
+	document.getElementsByTagName("li")[i].style.transform = "scale(1.5,1.5)";
+	document.getElementsByTagName("li")[i].style.backgroundColor = "#626262";
 }
 function arrageImg(){
-
+	var gallery = document.getElementById("gallery");
+	var ul = document.getElementById("gallery").getElementsByTagName("ul")[0];
 	for(var i=0, len=imgData.length; i<len; i++){
 		var imgcell = document.createElement("div");
 		imgcell.setAttribute("class", "cell");
+		imgcell.title = i+1;
 		var front = document.createElement("div");
 		front.setAttribute("class", "side front");
 		var back = document.createElement("div");
@@ -47,6 +37,9 @@ function arrageImg(){
 		var h4 = document.createElement("h4");
 		h4.textContent = imgData[i].desc;
 
+		var li = document.createElement("li");
+		li.title = 1+i;
+
 		front.appendChild(img);
 		front.appendChild(h3);
 		back.appendChild(h4);
@@ -54,12 +47,14 @@ function arrageImg(){
 		imgcell.appendChild(back);
 
 		
-		imgcell.style.top = (Math.random()*height-130)+"px";
-		imgcell.style.left = ((Math.random()>0.5?(width/2+380):0)+(Math.random()*(width/2-380)-190))+"px";
-		imgcell.style.transform = "rotate("+(Math.random()>0.5?"":"-")+Math.random()*45+"deg)";
+		var postion = getRandomResult();
+		imgcell.style.top = postion.top+"px";
+		imgcell.style.left = postion.left+"px";
+		imgcell.style.transform = "rotate("+(Math.random()>0.5?"":"-")+Math.random()*30+"deg)";
 	
 		
 		gallery.appendChild(imgcell);
+		ul.appendChild(li);
 	}
 
 	placeCenter(centerpic);
@@ -67,43 +62,71 @@ function arrageImg(){
 
 function posUpdate(){
 	var imgcell = document.getElementsByClassName("cell");
+	var navs = document.getElementById("gallery").getElementsByTagName("li");
+	var pos = getRandomResult();
 	for(var i=0, len = imgcell.length; i<len; i++){
 		if( i===centerpic){
-			imgs[i].style.top = (height/2 - 150)+"px";
-			imgs[i].style.left = (width/2 - 190)+"px";
-			imgs[i].style.transform = "rotate(0deg)";
-			imgs[i].style.zIndex = 100;
+			placeCenter(i);
 		}else{
-			imgcell[i].style.top = (Math.random()*height-130)+"px";
-			imgcell[i].style.left = ((Math.random()>0.5?(width/2+380):0)+(Math.random()*(width/2-380)-190))+"px";
-			imgcell[i].style.transform = "rotate("+(Math.random()>0.5?"":"-")+Math.random()*45+"deg)";
+			pos = getRandomResult();
+			imgcell[i].style.top = pos.top+"px";
+			imgcell[i].style.left = pos.left+"px";
+			imgcell[i].style.transform = "rotate("+(Math.random()>0.5?"":"-")+Math.random()*30+"deg)";
+			navs[i].style.transform = "scale(1,1)";
+			navs[i].style.backgroundColor = "rgba(0,0,0,0)";
 		}
 		
 	}
 }
 
+function getRandomResult(){
+	var top = Math.random()*height-pic_height*0.5;
+	var left = Math.random()*width-pic_width*0.5;
+
+	var minLeft = width*0.5-pic_width*1.5,
+		maxLeft = width*0.5+pic_width*0.5,
+		minTop = height*0.5-pic_height*1.5,
+		maxTop = height*0.5+pic_width*0.5;
+
+	while( (top>=minTop && top<=maxTop) && (left>=minLeft && left<=maxLeft)){
+		top = Math.random()*height-pic_height*0.5;
+		left = Math.random()*width-pic_width*0.5;
+
+	}
+
+	return {top:top, left:left};
+}
 function addEvent(){
 	var imgcell = document.getElementsByClassName("cell");
+	var navs = document.getElementById("gallery").getElementsByTagName("li");
 	for(var i=0, len=imgcell.length; i<len; i++){
-		imgcell[i].addEventListener("click", function(){
-			for(var j=0; j<len; j++){
-				if(imgcell[j] ===this){
-					if( j===centerpic){
-						if(isback){
-							this.style.transform = "translate(0,0) rotate(0deg)";
-						}else{
-							this.style.transform = "translate(40%, 0) rotateY(180deg)";	
-						}
-						isback = !isback;
-					}else{
-						centerpic = j;
-						posUpdate();
-						this.style.transform = "translate(40%, 0) rotateY(180deg)";
-						isback = true;
-					}
-				}
-			}
-				
+		imgcell[i].addEventListener("click", fanzhuan);
+		navs[i].addEventListener("click", function(){
+			imgcell[this.title-1].click();
 		})
 	}
+
+	
+}
+
+function fanzhuan(){
+	var navs = document.getElementById("gallery").getElementsByTagName("li");
+	if( this.title-1 === centerpic){
+		if(isback){
+			this.style.transform = "translate(0,0) rotate(0deg)";
+			navs[centerpic].style.transform = "scale(1.5,1.5) rotateY(0deg)";
+		}else{
+			this.style.transform = "translate(40%, 0) rotateY(180deg)";	
+			navs[centerpic].style.transform = "scale(1.5,1.5) rotateY(180deg)";
+		}
+		isback = !isback;
+	}
+	else{
+		centerpic = this.title-1;
+		posUpdate();
+		this.style.transform = "translate(40%, 0) rotateY(180deg)";
+		navs[centerpic].style.transform = "scale(1.5,1.5) rotateY(180deg)";
+		isback = true;
+	}
+	
 }
